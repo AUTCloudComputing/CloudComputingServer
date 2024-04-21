a59990479232e02043e849c32b774c5e4578e0ae
yangguangliang <guangliang.yang@hotmail.com>
2024-04-21 18:17:58 +1200

    swagger added

--- booking-system/src/main/java/ac/aut/CloudComputing/bookingsystem/controller/OrderController.java
+++ booking-system/src/main/java/ac/aut/CloudComputing/bookingsystem/controller/OrderController.java
@@ -1,5 +1,4 @@
 package ac.aut.CloudComputing.bookingsystem.controller;
- 
 
 import ac.aut.CloudComputing.bookingsystem.dto.OrderDTO;
 import ac.aut.CloudComputing.bookingsystem.model.Order;
@@ -10,8 +9,13 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 @RestController
 @RequestMapping("/api/orders")
+@Api(tags = "Order Management")
 public class OrderController {
 
     private final OrderService orderService;
@@ -21,19 +25,22 @@ public class OrderController {
     }
 
     @GetMapping
+    @ApiOperation(value = "Get all orders")
     public ResponseEntity<List<OrderDTO>> getAllOrders() {
     	List<OrderDTO> orders = orderService.getAllOrders();
         return ResponseEntity.ok(orders);
     }
 
     @PostMapping
-    public ResponseEntity<OrderDTO> createOrder(@RequestBody OrderDTO orderRequest) {
+    @ApiOperation(value = "Create an order")
+    public ResponseEntity<OrderDTO> createOrder(@RequestBody @ApiParam("Order details") OrderDTO orderRequest) {
     	OrderDTO order = orderService.createOrder(orderRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(order);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<OrderDTO> getOrderById(@PathVariable String orderId) {
+    @ApiOperation(value = "Get order by ID")
+    public ResponseEntity<OrderDTO> getOrderById(@PathVariable @ApiParam("Order ID") String orderId) {
     	OrderDTO order = orderService.getOrderById(orderId);
         if (order != null) {
             return ResponseEntity.ok(order);
@@ -43,7 +50,8 @@ public class OrderController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<OrderDTO> updateOrder(@PathVariable String orderId, @RequestBody OrderDTO orderRequest) {
+    @ApiOperation(value = "Update an order")
+    public ResponseEntity<OrderDTO> updateOrder(@PathVariable @ApiParam("Order ID") String orderId, @RequestBody @ApiParam("Updated order details") OrderDTO orderRequest) {
     	OrderDTO updatedOrder = orderService.updateOrder(orderId, orderRequest);
         if (updatedOrder != null) {
             return ResponseEntity.ok(updatedOrder);
@@ -52,13 +60,10 @@ public class OrderController {
         }
     }
 
-
-    
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteOrder(@PathVariable String orderId) {
+    @ApiOperation(value = "Delete an order")
+    public ResponseEntity<Void> deleteOrder(@PathVariable @ApiParam("Order ID") String orderId) {
         orderService.deleteOrder(orderId);
         return ResponseEntity.noContent().build();
     }
-    
 }
-
