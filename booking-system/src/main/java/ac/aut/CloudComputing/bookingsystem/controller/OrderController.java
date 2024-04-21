6e28e7aa3870d09b0e291ae9a95fb86423772768
yangguangliang <guangliang.yang@hotmail.com>
2024-04-21 18:37:38 +1200

    tuning swagger

--- booking-system/src/main/java/ac/aut/CloudComputing/bookingsystem/controller/OrderController.java
+++ booking-system/src/main/java/ac/aut/CloudComputing/bookingsystem/controller/OrderController.java
@@ -1,7 +1,6 @@
 package ac.aut.CloudComputing.bookingsystem.controller;
 
-import ac.aut.CloudComputing.bookingsystem.dto.OrderDTO;
-import ac.aut.CloudComputing.bookingsystem.model.Order;
+import ac.aut.CloudComputing.bookingsystem.dto.OrderDTO; 
 import ac.aut.CloudComputing.bookingsystem.service.OrderService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- booking-system/src/main/java/ac/aut/CloudComputing/bookingsystem/dto/UserLoginDTO.java
+++ booking-system/src/main/java/ac/aut/CloudComputing/bookingsystem/dto/UserLoginDTO.java
@@ -3,6 +3,7 @@ package ac.aut.CloudComputing.bookingsystem.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+@Schema
 @Data
 public class UserLoginDTO {
 
