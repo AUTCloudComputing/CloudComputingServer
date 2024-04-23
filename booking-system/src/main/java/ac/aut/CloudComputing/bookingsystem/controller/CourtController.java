9acf802c9d1d890baf1b3b2aa68b0ce48ed74c36
yangguangliang <guangliang.yang@hotmail.com>
2024-04-23 22:47:57 +1200

    check the model integrity

--- booking-system/src/main/java/ac/aut/CloudComputing/bookingsystem/controller/CourtController.java
+++ booking-system/src/main/java/ac/aut/CloudComputing/bookingsystem/controller/CourtController.java
@@ -1,11 +1,14 @@
 package ac.aut.CloudComputing.bookingsystem.controller;
 
-import ac.aut.CloudComputing.bookingsystem.dto.CourtDTO; 
+import ac.aut.CloudComputing.bookingsystem.dto.CourtReqDTO;
+import ac.aut.CloudComputing.bookingsystem.dto.CourtRspDTO; 
 import ac.aut.CloudComputing.bookingsystem.service.CourtService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 import io.swagger.annotations.Api;
@@ -25,15 +28,28 @@ public class CourtController {
 
     @GetMapping
     @ApiOperation(value = "Get all courts")
-    public ResponseEntity<List<CourtDTO>> getAllCourts() {  
-    	List<CourtDTO> courts = courtService.getAllCourts();
+    public ResponseEntity<List<CourtRspDTO>> getAllCourts() {  
+    	List<CourtRspDTO> courts = courtService.getAllCourts();
         return ResponseEntity.ok(courts);
     } 
 
     @PostMapping
     @ApiOperation(value = "Create an court")
-    public ResponseEntity<CourtDTO> createCourt(@RequestBody @ApiParam("court details") CourtDTO courtRequest) {
-    	CourtDTO court = courtService.createCourt(courtRequest);
+    public ResponseEntity<CourtRspDTO> createCourt(
+
+            @RequestParam("courtName") String courtName,
+            @RequestParam("imageFile") MultipartFile image,
+            @RequestParam("status") int status,
+            @RequestParam("description") String description
+            ) throws IOException {
+    	
+    	 CourtReqDTO courtRequest = new CourtReqDTO();
+    	    courtRequest.setCourtName(courtName);
+    	    courtRequest.setImageFile(image);
+    	    courtRequest.setStatus(status);
+    	    courtRequest.setDescription(description);
+    	    
+    	CourtRspDTO court = courtService.createCourt(courtRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(court);
     }
 
@@ -47,8 +63,8 @@ public class CourtController {
     
     @GetMapping("/{courtId}")
     @ApiOperation(value = "Get court by ID")
-    public ResponseEntity<CourtDTO> getcourtById(@PathVariable @ApiParam("court ID") String courtId) {
-    	CourtDTO court = courtService.getCourtById(courtId);
+    public ResponseEntity<CourtRspDTO> getcourtById(@PathVariable @ApiParam("court ID") String courtId) {
+    	CourtRspDTO court = courtService.getCourtById(courtId);
         if (court != null) {
             return ResponseEntity.ok(court);
         } else {
@@ -58,8 +74,21 @@ public class CourtController {
 
     @PutMapping("/{courtId}")
     @ApiOperation(value = "Update an court")
-    public ResponseEntity<CourtDTO> updateCourt(@PathVariable @ApiParam("court ID") String courtId, @RequestBody @ApiParam("Updated court details") CourtDTO courtRequest) {
-    	CourtDTO updatedcourt = courtService.updateCourt(courtId, courtRequest);
+    public ResponseEntity<CourtRspDTO> updateCourt(@PathVariable @ApiParam("court ID") String courtId
+    		,
+            @RequestParam("courtName") String courtName,
+            @RequestParam("imageFile") MultipartFile image,
+            @RequestParam("status") int status,
+            @RequestParam("description") String description
+            ) throws IOException {
+    	
+    	 CourtReqDTO courtRequest = new CourtReqDTO();
+    	    courtRequest.setCourtName(courtName);
+    	    courtRequest.setImageFile(image);
+    	    courtRequest.setStatus(status);
+    	    courtRequest.setDescription(description);
+    	    
+    	CourtRspDTO updatedcourt = courtService.updateCourt(courtId, courtRequest);
         if (updatedcourt != null) {
             return ResponseEntity.ok(updatedcourt);
         } else {
--- booking-system/src/main/java/ac/aut/CloudComputing/bookingsystem/mapper/CourtMapper.java
+++ booking-system/src/main/java/ac/aut/CloudComputing/bookingsystem/mapper/CourtMapper.java
@@ -3,15 +3,17 @@ package ac.aut.CloudComputing.bookingsystem.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-import ac.aut.CloudComputing.bookingsystem.dto.CourtDTO; 
+import ac.aut.CloudComputing.bookingsystem.dto.CourtReqDTO;
+import ac.aut.CloudComputing.bookingsystem.dto.CourtRspDTO;
 import ac.aut.CloudComputing.bookingsystem.model.Court;
 
 @Mapper
 public interface CourtMapper {
 	
     CourtMapper INSTANCE = Mappers.getMapper(CourtMapper.class);
- 
-    CourtDTO CourtToCourtDTO(Court court);
+
+    CourtRspDTO Court2CourtRspDTO(Court court);
+    Court CourtReqDTO2Court(CourtReqDTO req);
      
      
      
