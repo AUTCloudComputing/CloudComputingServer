dd53ec4beba39eda26d218c67487fabb23dd4351
ChenYilong <luohanchenyilong@163.com>
2024-04-29 21:29:35 +1200

    use ngix to fix cross-domain problem

--- booking-system-maven/booking_system_backend/target/generated-sources/annotations/ac/aut/CloudComputing/bookingsystem/mapper/OrderMapperImpl.java
+++ booking-system-maven/booking_system_backend/target/generated-sources/annotations/ac/aut/CloudComputing/bookingsystem/mapper/OrderMapperImpl.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-04-28T13:19:05+1200",
+    date = "2024-04-29T17:24:39+1200",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.3 (Oracle Corporation)"
 )
 public class OrderMapperImpl implements OrderMapper {
